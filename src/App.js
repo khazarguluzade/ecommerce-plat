3616d87f530de81b49ffdada89f3ba61f01dcda1
Khazar Guluzade <xezer.quluzade95@gmail.com>
2020-07-09 12:08:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import HomePage from "./Components/HomePage/homepage";
+
+function App() {
+  return <div>
+    <HomePage />
+  </div>;
+}
+
+export default App; 

fc1d03ab5beaad7c0dc9af6feffe4160d25e6e22
Khazar Guluzade <xezer.quluzade95@gmail.com>
2020-07-10 11:32:05 +0300

    adding shoppage and components for collection

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -11,29 +11,29 @@ class Directory extends React.Component {
         {
           title: "ŞAPKA",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          id: 1
+          id: 1,
         },
         {
           title: "CEKET",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          id: 2
+          id: 2,
         },
         {
-          title: "SPOR AYAKKABI",
+          title: "AYAKKABI",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          id: 3
+          id: 3,
         },
         {
           title: "KADIN",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           size: "large",
-          id: 4
+          id: 4,
         },
         {
           title: "ERKEK",
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
           size: "large",
-          id: 5
+          id: 5,
         },
       ],
     };
@@ -43,7 +43,7 @@ class Directory extends React.Component {
     console.log(this.state.sections);
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size}) => (
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
           <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
